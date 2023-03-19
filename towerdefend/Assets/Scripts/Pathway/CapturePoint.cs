838a1fa2cc7e3bad8f69b88b1e4be837d784b60d
hungng1301 <90265514+hungng1301@users.noreply.github.com>
2023-03-20 01:09:16 +0700

    build tower

---
+++ towerdefend/Assets/Scripts/Pathway/CapturePoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// If enemy rise this point - player will be defeated.
+/// </summary>
+public class CapturePoint : MonoBehaviour
+{
+    // Enemy already reached capture point
+    public int alreadyCaptured = 5;
+    public Text HP;
+
+    /// <summary>
+    /// Raises the trigger enter2d event.
+    /// </summary>
+    /// <param name="other">Other.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // If collision allowed for this scene
+        if (LevelManager.IsCollisionValid(gameObject.tag, other.gameObject.tag) == true)
+        {
+            alreadyCaptured--;
+            HP.text = alreadyCaptured.ToString();
+            Destroy(other.gameObject);
+           
+            if (alreadyCaptured == 0)
+            {
+                //alreadyCaptured = true;
+                
+                EventManager.TriggerEvent("Captured", other.gameObject, null);
+            }
+        }
+    }
+}
